4e4bd5a63f9a29e083498644871239a4368b4d14
readyvibes <maranoncarlos126@gmail.com>
2026-03-05 23:19:10 -0500

    Remove unused imports and `throws IOException` from `Main` class. Simplify `MindReader` initialization.

--- src/main/java/com/mindreader/Main.java
+++ src/main/java/com/mindreader/Main.java
@@ -1,21 +1,9 @@
 package com.mindreader;
 
-
-import com.github.javaparser.StaticJavaParser;
-import com.github.javaparser.ast.CompilationUnit;
-import com.mindreader.graph.JavaGraphManager;
-import com.mindreader.utils.Node;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 public class Main {
     private static MindReader mindReader;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         mindReader = new MindReader("", false);
     }
-
-
 }
